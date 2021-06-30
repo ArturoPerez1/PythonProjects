33fc1cf03fb5476c74ca469375177bf2ff1687b5
Arturo Perez <arjperez@gmail.com>
2021-06-29 22:54:56 -0400

    feat: add my venv and other files

---
+++ mediumStage/list_and_dicts.py
@@ -0,0 +1,25 @@
+def run():
+    my_list = [1,"hello",True,4.5]
+
+    super_list = [
+        {
+        "firstname" : "Arturo",
+        "lastname" : "Perez"
+        },
+        {
+        "firstname" : "Arturo",
+        "lastname" : "Perez"
+        },
+
+    ]
+
+    super_dict = {
+        "natural_nums": [1,2,3,4,5],
+        "floating_nums": [1.5,1.6,0.5]
+    }
+
+    for lista in len(super_dict):name
+        print(lista)
+
+if __name__ == '__main__':
+    run()
